cf1e1def6b351d37cdd956cfb1c1c2673fd17d06
Saksham <145899772+sakksham27@users.noreply.github.com>
2025-06-07 19:34:04 -0400

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,27 @@
+/*
+
+Initialize DB: 
+
+  This script initializes the foundational structure for the Data Warehouse Project.
+  It creates the primary database "DataWarehouse", 
+  which contains three structured schemas:
+
+    • bronze – stores raw, ingested data
+    • silver – holds cleaned and validated data
+    • gold   – contains business-ready, aggregated data
+
+  These layers reflect the modern data lakehouse architecture
+  and enable efficient data transformation and analytics.
+
+*/
+
+
+
+-- Create the database: "DataWarehouse" 
+CREATE DATABASE DataWarehouse;
+
+-- Create bronze, silver, gold schemas
+CREATE SCHEMA IF NOT EXISTS bronze;
+CREATE SCHEMA IF NOT EXISTS silver;
+CREATE SCHEMA IF NOT EXISTS gold;
+
